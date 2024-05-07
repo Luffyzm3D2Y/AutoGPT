1685d7d9a1031e5f052e41e7834d5e5579563a00
youziming <youziming@modelbest.cn>
2024-05-08 01:20:12 +0800

    fix the bug of web_selenium:

--- autogpts/forge/forge/actions/web/web_selenium.py
+++ autogpts/forge/forge/actions/web/web_selenium.py
@@ -314,7 +314,9 @@ def open_page_in_browser(url: str) -> WebDriver:
 
     if selenium_web_browser == "firefox":
         if selenium_headless:
-            options.headless = True
+            # options.headless = True
+            options.add_argument("--no-sandbox")
+            options.add_argument("--headless")
             options.add_argument("--disable-gpu")
         driver = FirefoxDriver(
             service=GeckoDriverService(GeckoDriverManager().install()), options=options
